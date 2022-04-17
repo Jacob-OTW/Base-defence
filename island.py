f91c24384af3f8acc3c650953b9c949247797246
Jacob-OTW <Erdnussflipshow@gmail.com>
2022-04-17 10:31:45 +0200

    Fixed bug where "island" didnt have an image, this is because the name starts with "is", which is BS, but now its just called "land" and works fine.

--- island.py
+++ island.py
@@ -2,14 +2,16 @@ import pygame
 from settings import *
 
 
-class Island(pygame.sprite.Sprite):
-    def __int__(self):
+class island(pygame.sprite.Sprite):
+    def __init__(self):
         super().__init__()
         self.image = pygame.image.load('Assets/island.png').convert_alpha()
         self.rect = self.image.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
         pass
 
 
-island_group = pygame.sprite.GroupSingle(Island())
+land_group = pygame.sprite.Group()
+land_group.add(island())
--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import time
 import math
 
 from settings import *
-
-from Island import island_group
+from island import land_group
 
 
 def HandleKeys():
@@ -17,18 +16,31 @@ def HandleKeys():
             pass
 
 
+class Island(pygame.sprite.Sprite):
+    def __int__(self):
+        super().__init__()
+        self.image = pygame.image.load('Assets/island.png').convert_alpha()
+        self.rect = self.image.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+
+    def update(self):
+        pass
+
+
+island_group = pygame.sprite.GroupSingle(Island())
+
+
 def main():
     last_time = time.time()
     while True:
         frame_time = time.time() - last_time
         last_time = time.time()
         # Events
-        island_group.update()
+        land_group.update()
         HandleKeys()
 
         # Visual
         screen.fill((1, 201, 250))
-        island_group.draw(screen)
+        land_group.draw(screen)
 
         text2 = score_font.render(f"{round(frame_time * 1000)}ms", True, (255, 255, 255))
         screen.blit(text2, (100, 150))
